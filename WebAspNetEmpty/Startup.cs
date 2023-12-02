48572989999143154ad3ab774538f958ba2ab9a0
Serg1300 <kul3@yandex.ru>
2023-12-02 16:16:04 +0300

    Добавьте файлы проекта.

---
+++ WebAspNetEmpty/Startup.cs
@@ -0,0 +1,75 @@
+﻿namespace WebAspNetEmpty
+{
+    public class Startup
+    {
+        private static IWebHostEnvironment _env;
+        public IConfiguration _configuration;
+        public Startup(IWebHostEnvironment env, IConfiguration configuration)
+        {
+            _env = env;
+            _configuration = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // Метод вызывается средой ASP.NET.
+        // Используйте его для настройки конвейера запросов
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment()|| env.IsStaging())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            //Добавляем компонент для логирования запросов с использованием метода Use.
+            app.Use(async (context, next) =>
+            {
+                // Для логирования данных о запросе используем свойства объекта HttpContext
+                Console.WriteLine($"[{DateTime.Now}]: New request to http://{context.Request.Host.Value + context.Request.Path}");
+                await next.Invoke();
+            });
+
+            //Добавляем компонент с настройкой маршрутов для главной страницы
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync($"Welcome to the {env.ApplicationName}!");
+                });
+            });
+
+            // Все прочие страницы имеют отдельные обработчики
+            app.Map("/about", About);
+            app.Map("/config", Config);
+
+            // Обработчик для ошибки "страница не найдена"
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync($"Page not found");
+            });
+        }
+        /// <summary>
+        ///  Обработчик для страницы About
+        /// </summary>
+        private static void About(IApplicationBuilder app)
+        {
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync($"{_env.ApplicationName} - ASP.Net Core tutorial project");
+            });
+        }
+
+        /// <summary>
+        ///  Обработчик для главной страницы
+        /// </summary>
+        private static void Config(IApplicationBuilder app)
+        {
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync($"App name: {_env.ApplicationName}. App running configuration: {_env.EnvironmentName}");
+            });
+        }
+    }
+}
